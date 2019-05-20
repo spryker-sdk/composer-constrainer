2bd8acb1882b27fd5fb17b3887bf3f7816e17bc7
René Klatt <rene.klatt@spryker.com>
2019-05-20 07:36:58 +0000

    TE-3458 Updated command description.

--- src/SprykerSdk/Zed/ComposerConstrainer/Communication/Console/ComposerConstraintConsole.php
+++ src/SprykerSdk/Zed/ComposerConstrainer/Communication/Console/ComposerConstraintConsole.php
@@ -29,7 +29,7 @@ class ComposerConstraintConsole extends Console
     {
         $this
             ->setName(static::COMMAND_NAME)
-            ->setDescription('Updates composer constraint in projects.');
+            ->setDescription('Updates composer constraint in projects. When a module is extended on project level, this command will change ^ to ~ in the projects composer.json. This will make sure that a composer update will only pull the exact version or patch versions of it.');
 
         $this->addOption(static::OPTION_DRY_RUN, static::OPTION_DRY_RUN_SHORT, InputOption::VALUE_OPTIONAL, 'Use this option to validate your projects constraints.');
     }
